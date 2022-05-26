e294c0c01cd329285474a20a2cee7b71249b37c6
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-05-25 23:44:26 -0500

    commit tree 5cb65c305ce93280b362136cc7913b515ec095a1

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age ++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}*/
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+
+    return gifts
+}
+
+wrapGifts(gifts);*/
+
+const names = [];
+const response = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++ ) {
+        response.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+
+    return response;
+}
+
+writeCards(names)
+
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
